64b71d54e3b68d75d994b0dd4a14554c0fe7b603
huskyth <1622313177@qq.com>
2024-02-01 14:42:21 +0800

    feat: 7x7 add

--- watermelon_chess/common.py
+++ watermelon_chess/common.py
@@ -1,5 +1,7 @@
 import json
 
+import numpy
+
 from watermelon_chess import data
 from watermelon_chess.control import getNeighboors
 
@@ -28,3 +30,19 @@ for from_point in range(21):
 for idx, move_string in enumerate(MOVE_LIST):
     MOVE_TO_INDEX_DICT[move_string] = idx
     INDEX_TO_MOVE_DICT[idx] = move_string
+
+# TODO://对于7x7的矩阵映射关系
+ARRAY_TO_IMAGE = {
+    0: (0, 3), 15: (6, 3), 6: (3, 0), 10: (3, 6),
+    1: (0, 2), 3: (0, 4), 2: (1, 3),
+    4: (2, 0), 7: (4, 0), 5: (3, 1),
+    8: (2, 6), 9: (3, 5), 11: (4, 6),
+    12: (5, 3), 13: (6, 2), 14: (6, 4),
+    20: (3, 3),
+    16: (2, 3), 17: (3, 2), 18: (3, 4), 19: (4, 3)
+}
+
+
+def from_array_to_input_tensor(numpy_array):
+    assert len(numpy_array) == 21
+    assert isinstance(numpy_array, numpy.ndarray)

0e8460f47fde7e2b441e05c8892bbdf4a928d31c
huskyth <1622313177@qq.com>
2024-02-05 14:09:02 +0800

    feat: save temp

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
 args = dotdict({
     'numIters': 1000,
     'numEps': 100,  # Number of complete self-play games to simulate during a new iteration.
-    'tempThreshold': 100,  #
+    'tempThreshold': 150,  #
     'updateThreshold': 0.6,
     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 200000,  # Number of game examples to train the neural networks.
@@ -24,7 +24,7 @@ args = dotdict({
     'arenaCompare': 40,  # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 1,
 
-    'checkpoint': './temp/',
+    'checkpoint': str(ROOT_PATH / "temp"),
     'load_model': False,
     'load_folder_file': (str(ROOT_PATH / "temp"), 'best.pth.tar'),
     'numItersForTrainExamplesHistory': 20,
